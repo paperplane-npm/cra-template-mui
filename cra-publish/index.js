66710b4f47dbd6df1c272bd0fe75dfa81615fba7
jia-niang <1@paperplane.cc>
2023-08-28 06:50:08 +0800

    chore(cra-publish): 现在会避免生成 cra 模板时带有一些不该有的目录

--- cra-publish/index.js
+++ cra-publish/index.js
@@ -17,6 +17,7 @@ delete newPackageJson.scripts['publish']
 fs.writeFileSync('./template.json', JSON.stringify({ package: newPackageJson }, null, 2))
 
 // 拷贝目录到 /template
+fs.rmSync('./template', { recursive: true, force: true })
 fs.mkdirSync('./template', { recursive: true })
 const exclude = [
   'template',
@@ -29,6 +30,11 @@ const exclude = [
   'package.json',
   'yarn.lock',
   'package-lock.json',
+  '.git',
+  '.DS_Store',
+  '.idea',
+  '.vscode',
+  'Thumbs.db',
 ]
 const items = fs.readdirSync('./')
 items.forEach(f => {
