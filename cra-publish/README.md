6ac6c590322e84290df680d8d445839827bcee50
jia-niang <1@paperplane.cc>
2023-08-28 10:02:40 +0800

    chore(deps+publish+cra-publish): 现在使用 release-it 进行版本发布；将 compileOptions.paths 移出 tsconfig.json 避免被删；更新构建脚本

---
+++ cra-publish/README.md
@@ -0,0 +1,16 @@
+# @paperplane/cra-template-mui
+
+# 指令
+
+```bash
+# 开发调试
+yarn dev
+# 或
+yarn start
+
+# 打包编译
+yarn build
+
+# 弹出配置
+yarn eject
+```
--- cra-publish/index.js
+++ cra-publish/index.js
@@ -9,41 +9,39 @@ for (const [key, value] of Object.entries(packageJson)) {
     newPackageJson[key] = value
   }
 }
-delete newPackageJson.devDependencies['lerna']
+delete newPackageJson.devDependencies['release-it']
 delete newPackageJson.devDependencies['fs-extra']
 delete newPackageJson.scripts['prepublishOnly']
-delete newPackageJson.scripts['publish']
+delete newPackageJson.scripts['release']
 
 fs.writeFileSync('./template.json', JSON.stringify({ package: newPackageJson }, null, 2))
 
 // 拷贝目录到 /template
 fs.rmSync('./template', { recursive: true, force: true })
 fs.mkdirSync('./template', { recursive: true })
-const exclude = [
-  'template',
-  'template.json',
-  'node_modules',
-  'cra-publish',
-  'lerna.json',
-  '.drone.yml',
-  '.gitignore',
-  'package.json',
-  '.npmrc',
-  '.yarnrc',
-  'yarn.lock',
-  'package-lock.json',
-  '.git',
-  '.DS_Store',
-  '.idea',
-  '.vscode',
-  'Thumbs.db',
-]
+
 const items = fs.readdirSync('./')
 items.forEach(f => {
-  if (!exclude.includes(f)) {
+  if (
+    [
+      'public',
+      'src',
+      '.editorconfig',
+      '.env',
+      '.eslintrc',
+      '.gitattributes',
+      '.prettierrc',
+      'config-overrides.js',
+      'LICENSE',
+      'tsconfig.json',
+      'tsconfig.paths.json',
+    ].includes(f)
+  ) {
     fs.copySync(`./${f}`, `./template/${f}`, { overwrite: true, preserveTimestamps: true })
   }
 })
+
+fs.copySync(`./cra-publish/README.md`, `./template/README.md`)
 
 // 处理 .gitignore
 const gitignore = String(fs.readFileSync('./.gitignore'))
